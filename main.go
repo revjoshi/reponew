6b43538d02b4248fc899ab70ce972db1be372218
revjoshi <revtijoshi1998@gmail.com>
2023-06-16 12:43:11 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+
+	"./controller"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/getEmployee", controller.AllEmployee).Methods("GET")
+	router.HandleFunc("/insertEmployee", controller.InsertEmployee).Methods("POST")
+	http.Handle("/", router)
+	fmt.Println("Connected to port 1234")
+	log.Fatal(http.ListenAndServe(":1234", router))
+}
